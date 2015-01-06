b1fd580b2a476bab088467534e04d76f266be0f2
Nikolay <nikssa23@gmail.com>
2015-01-06 02:16:11 +0200

    delete some files

--- app/js/controllers/PublishNewAddController.js
+++ app/js/controllers/PublishNewAddController.js
@@ -1,6 +1,10 @@
 adsApp.controller('PublishNewAddController', function PublishNewAddController($scope, adsRequester) {
     $scope.publishAd = function (ad) {
         console.log(ad);
-        adsRequester.publishNewAd(ad);
+        adsRequester.publishNewAd(ad).$promise.then(function (data) {
+            console.log(data);
+        }, function (error) {
+            console.log(error.data.message);
+        })
     }
 });
